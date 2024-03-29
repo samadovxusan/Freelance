30e4ce6a0866270c931da80a12f01cf03e7089f4
Husanboy <samadovxusan9013@gmail.com>
2024-03-29 15:15:30 +0500

    add JWT

---
+++ Lorby/Lorby.Api/Controller/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Persistence.Repositories.Interface;
+
+namespace Lorby.Api.Controller;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AuthController(IAuthService authService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Register(Register register)
+    {
+        var result = await authService.Register(register);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(Login login)
+    {
+        var result = await authService.Login(login);
+        return Ok(result);
+    }
+
+}
--- Lorby/Lorby.Infrastructure/Common/Entity/Services/AuthService.cs
+++ Lorby/Lorby.Infrastructure/Common/Entity/Services/AuthService.cs
@@ -1,17 +1,37 @@
-﻿using Domain.Entities;
+﻿using AutoMapper;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Persistence.DataContext;
 using Persistence.Repositories.Interface;
 
 namespace Infrastructure.Common.Entity.Services;
 
-public class AuthService: IAuthService
+public class AuthService(AppDbContext dbContext, IMapper mapper, IConfiguration configuration): IAuthService
 {
-    public ValueTask<bool> Register(Register register)
+    public async  ValueTask<bool> Register(Register register)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var user = mapper.Map<User>(register);
+            await dbContext.Users.AddAsync(user);
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 
-    public ValueTask<string> Login(Login login)
+    public async ValueTask<string> Login(Login login)
     {
-        throw new NotImplementedException();
+        var newUser = await dbContext.Users.FirstOrDefaultAsync(x => x.Password == login.Password && x.Email == login.Email);
+        if(newUser == null) 
+        {
+            return "Not Faund User";
+        }
+        var jwtToken = new IdentityTokenGeneratorService(configuration);
+        var resust = jwtToken.GenerateToken(newUser);
+        return await resust;
     }
 }
