844b094940d8d780d9e60b733dd8a39ef469a395
Husanboy <samadovxusan9013@gmail.com>
2024-04-11 21:37:43 +0500

    appdb

---
+++ Identity/Src/Backend/Identity.Persistence/DbContext/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Persistence.DbContext;
+
+public class AppDbContext:Microsoft.EntityFrameworkCore.DbContext 
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+        
+}
